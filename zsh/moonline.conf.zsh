95c8451305a762b9bec2501a12844e73f993aa5f
kagamilove0707 <kagamilove0707@gmail.com>
2014-04-05 22:55:02 +0900

    update moonline.zsh

--- zsh/moonline.conf.zsh
+++ zsh/moonline.conf.zsh
@@ -1,3 +1,7 @@
+# moonline.zshの開発時は読み込まないようにします
+# (シェル変数などがややこしくなるので)
+[ "$MOONLINE_DEVELOP" != '' ] && return 0
+
 # moonline.zshの読み込み
 test -f $DOTFILES/zsh/moonline.zsh/moonline.zsh && source $DOTFILES/zsh/moonline.zsh/moonline.zsh
 
@@ -10,11 +14,12 @@ MOONLINE_SUBSEPARATOR=($'\u2b81' $'\u2b83')
 
 MOONLINE_COMPONENTS+=(
   prompt "%%"
+  path "%30<...<%~%<<"
 )
 
 MOONLINE_LEFT_LINE=2
-MOONLINE_LEFT1_1=(user)
-MOONLINE_LEFT1_2=(current_directory)
+MOONLINE_LEFT1_1=(username)
+MOONLINE_LEFT1_2=(path)
 MOONLINE_LEFT2_1=(prompt)
 MOONLINE_RIGHT1=(date)
 
