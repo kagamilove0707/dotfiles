7d583cb7fbbfbf391262039c5b768c002075b292
kagamilove0707 <kagamilove0707@gmail.com>
2014-04-07 16:47:42 +0900

    fix git-info using array

--- zsh/moonline.conf.zsh
+++ zsh/moonline.conf.zsh
@@ -32,16 +32,16 @@ my-git-info() {
     branch=${branch:t}
     local str=$'\u2b60 '
     local sts mgd
-    sts=$(git status -s | wc -l)
-    mgd=$(git branch --no-merged | wc -l)
+    sts=(${(@f)"$(git status -s)"})
+    mgd=(${(@f)"$(git branch --no-merged)"})
     str+="%{"
-    if [[ $mgd -gt 0 ]]; then str+="%F{red}"
-    elif [[ $sts -gt 0 ]]; then str+="%F{yellow}"
+    if [[ $#mgd -gt 0 ]]; then str+="%F{red}"
+    elif [[ $#sts -gt 0 ]]; then str+="%F{yellow}"
     else str+="%F{green}" fi
     str+="%}"
     str+="$branch"
-    [[ $mgd -ne 0 ]] && str+=" M:$mgd"
-    [[ $sts -ne 0 ]] && str+=" F:$sts"
+    [[ $#mgd -ne 0 ]] && str+=" M:$#mgd"
+    [[ $#sts -ne 0 ]] && str+=" F:$#sts"
     print $str
   fi
 }
